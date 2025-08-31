1eb026ff2a2922fc9dca4c56c14169965dcc8d61
vishnuminnu <vishnuvardhan123987@gmail.com>
2025-08-31 12:52:14 +0530

    toml

--- src/components/Maincontent.jsx
+++ src/components/Maincontent.jsx
@@ -20,6 +20,14 @@ const Maincontent = () => {
 
   return (
     <>
+       <Helmet>
+        <title>My Website - Home</title>
+        <meta name="description" content="Welcome to My Website. Discover our products, features, and vision." />
+        <meta name="keywords" content="React, Products, Features, Vision, Contact" />
+        <meta property="og:title" content="My Website" />
+        <meta property="og:description" content="Discover our products, features, and vision." />
+        <meta property="og:type" content="website" />
+      </Helmet>
       <Herosection/>
       <Features/>
       <Products/>
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './components/ThemeContext.jsx'
-
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-    <div className="relative">
-      <App />
-     
-    </div>
-    </ThemeProvider>
-  </StrictMode>,
+    <HelmetProvider>
+      <ThemeProvider>
+        <div className="relative">
+          <App />
+        </div>
+      </ThemeProvider>
+    </HelmetProvider>
+  </StrictMode>
 )
